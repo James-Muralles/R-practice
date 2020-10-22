9a67958f6018269577876c65faee9dfd51c711f8
James-Muralles <james.muralles23@gmail.com>
2020-10-22 11:30:40 -0500

    more plot practice

--- test.R
+++ test.R
@@ -150,5 +150,53 @@ plot(1:8, las=2, ylab="ylabs", xlab="xlab", main="LAS = 2")
 plot(1:8, las=3, ylab="ylabs", xlab="xlab", main="LAS = 3")
 
 
+colors()
+?pch
+
+x=2:4
+plot(x, pch="c")
+plot(x, pch=13)
+
+par(mfrow=c(1,1), col.axis="black")
+
+library(plotrix)
+
+plot(1:7, ylab="", main="Line Types lty 0:6", xlab="lty 0:6")
+ablineclip(v=1, lty=1, col="sienna2", lwd=2)
+ablineclip(v=2, lty=2, col="sienna2", lwd=2)
+ablineclip(v=3, lty=3, col="sienna2", lwd=2)
+ablineclip(v=4, lty=4, col="sienna2", lwd=2)
+ablineclip(v=5, lty=5, col="sienna2", lwd=2)
+ablineclip(v=6, lty=6, col="sienna2", lwd=5)
+ablineclip(v=7, lty=0, col="sienna2", lwd=2)
+
+
+
+#Plotting things and setting parameters!!!!
+
+par(mar=c(4,3,3,3), col.axis="darkgreen")
+plot(cars$speed, type="s", col="red", bty="n", xlab="Cars ID", ylab="")
+text(8, 14, "Speed in mph", cex=0.85, col="red")
+
+par(new=T)#----------->two lines in one plot!!!
+plot(cars$dist, type="s", bty="n", ann=F,axes=F, col="darkblue")
+axis(side=4, col="darkblue")
+text(37,18, "Stopping distance in ft",cex=0.85, col="darkblue")
+title(main="Speed and stopping\n Distance of cars")
+
+
+?rivers
+View(rivers)
+rivers
+x=1:141
+y=rivers
+plot(rivers,pch=21, bg="green" , col="green", 
+     ylab="length in miles", 
+     xlab="",
+     main="Length of\n Major N American Rivers",
+     col.main="red")
+
+
+
 
 
